4eb27b5322f805672fab2cb3979ffac54eea7a40
Anurag Singh <abc103498@gmail.com>
2025-08-12 16:50:04 +0530

    logo functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Switcher13 from './Switcher13'
 function Navbar() {
   return (
     <div className='w-full p-4 flex justify-between'>
-        <div className='text-4xl font-bold dark:text-teal-300'>UI<span className='dark:text-orange-500'>Gen</span></div>
+        <div className='text-4xl font-bold dark:text-teal-300'><a href="/">UI<span className='dark:text-orange-500'>Gen</span></a></div>
         <div>
           <Switcher13 />
         </div>
