74a4ea2a966e3496de51f33bd913ec1d72ffa825
Anurag Singh <abc103498@gmail.com>
2025-08-12 16:23:39 +0530

    image fix

--- src/pages/Offline.jsx
+++ src/pages/Offline.jsx
@@ -1,9 +1,8 @@
-import offlineImage from '/ofimg.png';
 
 function Offline() {
   return (
     <div className="flex h-screen w-full flex-col justify-center items-center bg-gradient-to-b from-slate-900 to-purple-900 text-white">
-        <img src={offlineImage} className='w-100' />
+        <img src="/ofimg.png" className='w-100' />
         <h1 className="text-5xl font-bold">No Internet</h1>
     </div>
   );
