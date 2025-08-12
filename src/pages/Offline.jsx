ea342dc0e839f9461277c77b56b6718015736032
Anurag Singh <abc103498@gmail.com>
2025-08-12 16:26:41 +0530

    image removal

--- src/pages/Offline.jsx
+++ src/pages/Offline.jsx
@@ -2,8 +2,8 @@
 function Offline() {
   return (
     <div className="flex h-screen w-full flex-col justify-center items-center bg-gradient-to-b from-slate-900 to-purple-900 text-white">
-        <img src="/ofimg.png" className='w-100' />
-        <h1 className="text-5xl font-bold">No Internet</h1>
+        <h1 className="text-5xl font-extrabold">No Internet</h1>
+        <p className="mt-10 text-xl font-bold">Please connect to the Internet</p>
     </div>
   );
 }
